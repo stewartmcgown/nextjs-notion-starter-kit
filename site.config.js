04dc4a50d7469fc84d5b72be7ac2f7bb36cdd320
Stewart McGown <stewartcallummcgown@gmail.com>
2021-03-05 15:32:24 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '99f2638ce01a4d9093a1b932ac3d998b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Stewart McGown',
+  domain: 'blog.mcgown.dev',
+  author: 'Stewart McGown',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Stuff I\'ve done with tech.',
+  socialImageTitle: 'Stewart McGown',
+  socialImageSubtitle: 'Tech stuff I\'ve done! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'stewartmcgown',
+  
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
